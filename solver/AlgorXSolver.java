0ffa3061a6b22100bfeff3fb969e3453b4a1a8e6
dpleiter <dpleiter@gmail.com>
2020-05-23 15:23:52 +1000

    initialise matrix with starting sudoku grid

--- solver/AlgorXSolver.java
+++ solver/AlgorXSolver.java
@@ -18,6 +18,10 @@ public class AlgorXSolver extends StdSudokuSolver {
     public boolean solve(SudokuGrid grid) {
         algXMatrix xMatrix = new algXMatrix(grid.getSize());
 
+        // xMatrix.printToFile();
+
+        xMatrix.init(grid);
+
         xMatrix.printToFile();
 
         // placeholder
@@ -29,6 +33,8 @@ public class AlgorXSolver extends StdSudokuSolver {
         private boolean[] colInclusion;
         private int[] colSums;
 
+        private boolean[] rowInclusion;
+
         int numRows;
         int numCols;
 
@@ -38,10 +44,9 @@ public class AlgorXSolver extends StdSudokuSolver {
 
             matrix = new boolean[numRows][numCols];
 
+            rowInclusion = new boolean[numRows];
             colInclusion = new boolean[numCols];
             colSums = new int[numCols];
-
-            double offset;
 
             for (int colNum = 0; colNum < numCols; colNum++) {
                 colInclusion[colNum] = true;
@@ -49,30 +54,77 @@ public class AlgorXSolver extends StdSudokuSolver {
             }
 
             for (int rowNum = 0; rowNum < numRows; rowNum++) {
+                rowInclusion[rowNum] = true;
+
                 // cell constraint
-                int cellCol = Math.floorDiv(rowNum, dimensions);
-                matrix[rowNum][cellCol] = true;
+                matrix[rowNum][cellConstraintByRow(rowNum, dimensions)] = true;
 
                 // row constraint
-                offset = dimensions * Math.floorDiv(rowNum, dimensions * dimensions) + rowNum % dimensions;
-                int rowCol = (dimensions * dimensions) + (int) offset;
-                matrix[rowNum][rowCol] = true;
+                matrix[rowNum][rowConstraintByRow(rowNum, dimensions)] = true;
 
                 // col constraint
-                offset = rowNum % (dimensions * dimensions);
-                int colCol = 2 * dimensions * dimensions + (int) offset;
-                matrix[rowNum][colCol] = true;
+                matrix[rowNum][colConstraintByRow(rowNum, dimensions)] = true;
 
                 // box constraint
-                offset = Math.pow(dimensions, 1.5) * Math.floorDiv(rowNum, (int) Math.pow(dimensions, 2.5))
-                        + dimensions
-                                * Math.floorDiv(rowNum % (dimensions * dimensions), (int) Math.pow(dimensions, 1.5))
-                        + rowNum % dimensions;
-
-                int boxCol = 3 * dimensions * dimensions + (int) offset;
-
-                matrix[rowNum][boxCol] = true;
+                matrix[rowNum][boxConstraintByRow(rowNum, dimensions)] = true;
             }
+        }
+
+        public void init(SudokuGrid grid) {
+            int gridSize = grid.getSize();
+
+            int cellConstraintCol;
+            int rowConstraintCol;
+            int colConstraintCol;
+            int boxConstraintCol;
+
+            for (int row = 0; row < gridSize; row++) {
+                for (int col = 0; col < gridSize; col++) {
+                    if (grid.getCellValue(row, col) != 0) {
+                        int mStartRow = (gridSize * gridSize * row) + (gridSize * col);
+                        for (int i = 0; i < gridSize; i++) {
+                            rowInclusion[mStartRow + i] = false;
+
+                            cellConstraintCol = cellConstraintByRow(mStartRow + i, gridSize);
+                            rowConstraintCol = rowConstraintByRow(mStartRow + i, gridSize);
+                            colConstraintCol = colConstraintByRow(mStartRow + i, gridSize);
+                            boxConstraintCol = boxConstraintByRow(mStartRow + i, gridSize);
+
+                            colSums[cellConstraintCol] = colSums[cellConstraintCol] - 1;
+                            colSums[rowConstraintCol] = colSums[rowConstraintCol] - 1;
+                            colSums[colConstraintCol] = colSums[colConstraintCol] - 1;
+                            colSums[boxConstraintCol] = colSums[boxConstraintCol] - 1;
+                        }
+
+                        colInclusion[cellConstraintByRow(mStartRow + grid.getCellValue(row, col) - 1,
+                                gridSize)] = false;
+                        colInclusion[rowConstraintByRow(mStartRow + grid.getCellValue(row, col) - 1, gridSize)] = false;
+                        colInclusion[colConstraintByRow(mStartRow + grid.getCellValue(row, col) - 1, gridSize)] = false;
+                        colInclusion[boxConstraintByRow(mStartRow + grid.getCellValue(row, col) - 1, gridSize)] = false;
+                    }
+                }
+            }
+        }
+
+        private int cellConstraintByRow(int rowNum, int dimensions) {
+            return Math.floorDiv(rowNum, dimensions);
+        }
+
+        private int rowConstraintByRow(int rowNum, int dimensions) {
+            return (dimensions * dimensions) + dimensions * Math.floorDiv(rowNum, dimensions * dimensions)
+                    + rowNum % dimensions;
+        }
+
+        private int colConstraintByRow(int rowNum, int dimensions) {
+            return 2 * dimensions * dimensions + rowNum % (dimensions * dimensions);
+        }
+
+        private int boxConstraintByRow(int rowNum, int dimensions) {
+            double offset = Math.pow(dimensions, 1.5) * Math.floorDiv(rowNum, (int) Math.pow(dimensions, 2.5))
+                    + dimensions * Math.floorDiv(rowNum % (dimensions * dimensions), (int) Math.pow(dimensions, 1.5))
+                    + rowNum % dimensions;
+
+            return 3 * dimensions * dimensions + (int) offset;
         }
 
         // For testing
@@ -81,12 +133,14 @@ public class AlgorXSolver extends StdSudokuSolver {
                 BufferedWriter file = new BufferedWriter(new FileWriter("out/matrix_out.csv"));
 
                 for (int row = 0; row < numRows; row++) {
-                    for (int col = 0; col < numCols; col++) {
-                        if (matrix[row][col]) {
-                            file.write("1");
-                        }
+                    if (rowInclusion[row]) {
+                        for (int col = 0; col < numCols; col++) {
+                            if (colInclusion[col] && matrix[row][col]) {
+                                file.write("1");
+                            }
 
-                        file.write(",");
+                            file.write(",");
+                        }
                     }
                     file.write("\n");
                 }
